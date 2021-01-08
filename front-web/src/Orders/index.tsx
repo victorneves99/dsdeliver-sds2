eb491e81aa6d6271d61171ca13de80973d4ec567
unknown <victorneves99@gmail.com>
2021-01-07 23:25:21 -0300

    API Integration

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,26 @@
-import './style.css';
-import { ReactComponent as Logo } from './logo.svg';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import './style.css';
+import { useEffect, useState } from 'react';
+
+import { Product } from './types';
+import { fetchProducts } from '../api';
+
 function Orders() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  console.log(products);
+
+  useEffect(() => {
+    fetchProducts()
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="orders-container">
       <StepsHeader />
+      <ProductsList products={products} />
     </div>
   );
 }
